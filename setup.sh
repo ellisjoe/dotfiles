44faffdd92be753b46a620e1b1b7a62b6ccde306
Joe Ellis <ellis125@gmail.com>
2014-08-31 02:23:49 -0700

    move themes to custom dir

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ git submodule init
 git submodule update
 
 # add my theme to the themes directory of oh-my-zsh
-cp my-clean.zsh-theme oh-my-zsh/themes
+cp my-clean.zsh-theme oh-my-zsh/custom/themes
 
 FILES=( gitconfig oh-my-zsh tmux.conf vim vimrc zshrc )
 # link everything that isn't a script
