be5b45a90423caba45d1c1fe6fbc5fce62319a73
Joe Ellis <ellis125@gmail.com>
2015-10-18 17:22:31 -0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,15 +9,15 @@ cp my-clean.zsh-theme oh-my-zsh/custom/themes
 
 cp -r zsh-syntax-highlighting oh-my-zsh/custom/plugins
 
+# link all config files
 FILES=( gitconfig oh-my-zsh tmux.conf vim vimrc zshrc )
-# link everything that isn't a script
 for DF in ${FILES[*]}; do
     echo "Linking $DF"
     rm -rf ~/.$DF
     ln -s $(pwd)/$DF ~/.$DF
 done
 
-ln -s $(pwd)/vimrc ~/.vrapperrc
+ln -sf $(pwd)/vimrc ~/.vrapperrc
 
 # install pathogen
 cp vim-pathogen/autoload/pathogen.vim vim/autoload/
